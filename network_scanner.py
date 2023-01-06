1e25db7d3a4f00217ae2c240ae150c50d1630b2e
atreus <atreus@nemesis.nemesis>
2023-01-06 02:36:54 +0100

    Pregunta a cada subnet quien tienen nuestra IP

--- network_scanner.py
+++ network_scanner.py
@@ -3,10 +3,11 @@ import scapy.all as scapy
 def scan(ip):
     #pregunta a la maquina quien tiene este IP
     arp_request = scapy.ARP(pdst = ip)
-    arp_request.show() #Parametros ARP
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
-    broadcast.show() #Parametros MAC
     arp_request_broadcast = broadcast/arp_request
+
+    #pregunta a cada subnet quien tiene nuestra IP
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
 
 
 #    scapy.arping(ip)
