1696d5b9a79c582f8cf8c2bb55935ed4baade98b
atreus <atreus@nemesis.nemesis>
2023-01-06 02:06:38 +0100

    añadida instruccion para conseguir IP_LOCAL

--- network_scanner.py
+++ network_scanner.py
@@ -3,5 +3,6 @@ import scapy.all as scapy
 def scan(ip):
     scapy.arping(ip)
 
-#ejecutar con sudo
-scan("IP_LOCAL/24")
+#comprobar antes con sudo route -n
+#ejecutar con SUDO
+scan("192.168.100.0/24")
