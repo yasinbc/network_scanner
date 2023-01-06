01b1b58279688b54f47315fd4cfc363e72b1a65e
atreus <atreus@nemesis.nemesis>
2023-01-06 02:14:48 +0100

    Lista el broadcast que podemos usar

--- network_scanner.py
+++ network_scanner.py
@@ -1,8 +1,13 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    #pregunta a la maquina quien tiene este IP
+    arp_request = scapy.ARP(pdst = ip)
+    broadcast = scapy.Ether()
+    scapy.ls(scapy.Ether)
+
+#    scapy.arping(ip)
 
 #comprobar antes con sudo route -n
 #ejecutar con SUDO
-scan("192.168.100.0/24")
+scan("192.168.100.1/24")
