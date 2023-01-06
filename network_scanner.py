a9fac0fe5b4812bc37c0f614c3b719a0a07afce8
atreus <atreus@nemesis.nemesis>
2023-01-06 03:03:19 +0100

    feel & touch de la interfaz

--- network_scanner.py
+++ network_scanner.py
@@ -6,15 +6,15 @@ def scan(ip):
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
     arp_request_broadcast = broadcast/arp_request
     #pregunta a cada subnet quien tiene nuestra IP
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
+    print("IP\t\t\tMAC address\n-----------------------------------------")
     for element in answered_list:
-        print("IP>  ["+element[1].psrc+"]")
-        print("MAC> ["+element[1].hwsrc+"]")
-        print("---------------------------------------------------")
+        print(element[1].psrc+"\t\t"+element[1].hwsrc)
+        print("-----------------------------------------")
 
 #    scapy.arping(ip)
 
 #comprobar antes con "sudo route -n"
 #ejecutar con SUDO
-scan("IP_ROUTER/24")
+scan("192.168.100.1/24")
